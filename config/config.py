f9015207ffc8cfe619473402c0c927e724a90c5f
Pablo Lombao <pablombao@gmail.com>
2024-02-27 09:15:04 +0000

    fix bug on config

--- config/config.py
+++ config/config.py
@@ -4,5 +4,5 @@
 QUERY_TRAINDATA = """SELECT * FROM `nifty-acolyte-415508.mlops_data.car_price_clean`"""
 TARGET = "Price"
 KEYS = "ID"
-FEATURES_CAT = ["Manufacturer", "Model", "Category", "Leather_interior", "Fuel_type","Gear_box_type"]]
+FEATURES_CAT = ["Manufacturer", "Model", "Category", "Leather_interior", "Fuel_type","Gear_box_type"]
 FEATURES_NUM = ["Prod__year", "Engine_volume", "Mileage", "Cylinders"]
